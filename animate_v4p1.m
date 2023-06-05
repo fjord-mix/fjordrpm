bc529528bfde2354bdcd75132b9da62cbf04f17c
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-06-05 14:48:44 +0100

    now received struct with outputs and path+file name to save figure instead

--- animate_v4p1.m
+++ animate_v4p1.m
@@ -1,8 +1,8 @@
 % function to animate box model results - updated to show more variables as well
-function [] = animate_v4p1(outputfile,fname,nframes)
+function [] = animate_v4p1(fjord_model,outputdir,outputfile,nframes)
 
-clearvars -except outputfile fname nframes; close all;
-load(outputfile,'fjord_model');
+% clearvars -except fjord_model outputfile nframes; close all;
+% load(outputfile,'fjord_model');
 
 names = fieldnames(fjord_model);
 for i=1:length(names)
@@ -10,8 +10,7 @@ for i=1:length(names)
 end
 
 orig_dir = pwd;
-folder = dir(outputfile);
-eval(['cd ',folder.folder,'/plots/']);
+eval(['cd ',outputdir,'/plots/']);
 
 Slims = [min([min(s.S(:)),min(f.Ss(:))]), max([max(s.S(:)),max(f.Ss(:))])];
 Tlims = [min([min(s.T(:)),min(f.Ts(:))]), max([max(s.T(:)),max(f.Ts(:))])];
@@ -144,13 +143,13 @@ for i=1:round((length(s.t)-1)/nframes):length(s.t)-1
         savenum = num2str(i);
     end
 
-    saveplot(25,10,300,[fname,'_',savenum,'.png']);
+    saveplot(25,10,300,[outputfile,'_',savenum,'.png']);
     close all;
 
 end
 
 % write video
-video = VideoWriter([fname,'.mp4'],'MPEG-4');
+video = VideoWriter([outputfile,'.mp4'],'MPEG-4');
 video.FrameRate = 10;
 open(video);
 for i=1:round((length(s.t)-1)/nframes):length(s.t)-1
@@ -161,12 +160,12 @@ for i=1:round((length(s.t)-1)/nframes):length(s.t)-1
         savenum = num2str(i);
     end
 
-    I = imread([fname,'_',savenum,'.png']);
+    I = imread([outputfile,'_',savenum,'.png']);
     writeVideo(video,I);
 
 end
 close(video);
-delete([fname,'*.png'])
+delete([outputfile,'*.png'])
 eval(['cd ',orig_dir]); 
 
 end
