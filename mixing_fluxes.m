6fc800bac5a4c4d1da695749b008e4d8f1782d6c
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-06-07 15:42:00 +0100

    mixing fluxes use wmax if stratification is unstable

--- mixing_fluxes.m
+++ mixing_fluxes.m
@@ -17,11 +17,18 @@ function [QVk0,QTk0,QSk0] = mixing_fluxes(H0,T0,S0,QVg0,QVs0,p)
             uu = (QVg0(k)+(QVs0(k)-QVg0(k))*x/p.L)/(p.W*H0(k));
             % buoyancy jump between boxes
             B = p.g*(p.betaS*(S0(k+1)-S0(k))-p.betaT*(T0(k+1)-T0(k)));
-            % richardson number
-            R = 0.5*(H0(k+1)+H0(k))*B./(ul-uu).^2;
-            % vertical entrainment velocity
-            % capped at 4e-5
-            w = min(p.K0*abs(ul-uu).*R.^(-0.75),p.wmax);
+
+            % an unstable stratification would yield complex velocities if
+            % based on the Richardson number
+            if B > 0 
+                % richardson number
+                R = 0.5*(H0(k+1)+H0(k))*B./(ul-uu).^2;
+                % vertical entrainment velocity
+                % capped at 4e-5
+                w = min(p.K0*abs(ul-uu).*R.^(-0.75),p.wmax);
+            else
+                w = 1.*ones(size(x)).*p.wmax; % so if unstable, we use the min mixing velocity
+            end
             % fluxes
             Q(k) = sign(mean(abs(uu)-abs(ul)))*p.W*trapz(x,w);
             QT(k) = (Q(k)>0)*Q(k)*T0(k+1) + (Q(k)<0)*Q(k)*T0(k);
