8583b8053a9048af74c9e0572c2fc3f2baa59952
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-04 22:50:33 +0100

    changed structure

--- run_boxmodel.m
+++ run_boxmodel.m
@@ -8,10 +8,18 @@ close all
 %% Name the experiment and create an output directory
 
 %% Load the problem setup file (parameters, initial conditions, forcings)
-addpath('../examples');
+%% Examples
+addpath('examples/');
+% Example 1: intermediate_circulation
+% Example 2:
+% Example 3:
 p = intermediate_circulation;
+
+%% time steps etc
 t = 0:p.dt:p.t_end;
 f = get_forcing(p, t);
 a = get_initial_conditions(p, f);
 
-%% Run the model and save the output 
+%% Run the model 
+% 
+%% and save the output 
