6994d800d14cf5958f7ec3018050ee23b5cf3d8f
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-04 12:51:07 +0100

    added new driver script run_boxmodel.m

---
+++ run_boxmodel.m
@@ -0,0 +1,12 @@
+%% run_boxmodel.m
+% Description here 
+
+%%
+clearvars
+close all
+
+%% Name the experiment and create an output directory
+
+%% Load the problem setup file (parameters, initial conditions, forcings)
+
+%% Run the model and save the output 
