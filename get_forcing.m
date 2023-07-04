778ffb8ed02dae04a81566c458daf73e2cb5df39
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-04 22:42:03 +0100

    added code to get_forcing.m

---
+++ get_forcing.m
@@ -0,0 +1,30 @@
+function f = get_forcing(p, t)
+
+% GET_FORCING  Obtain the forcing structure.
+%   F = GET_FORCING(P, T) gets the forcing structure F for the shelf, icebergs and
+% subglacial discharge for the given box model parameters P and T.
+
+%% Shelf forcing
+f.zs = -p.H:0; % will this break if p.H is not an integer?
+
+if p.zd = 0
+    f.zi = p.z0; % no oscillation
+else
+    f.zi = p.z0+(p.zd/2)*sin(2*pi*t/p.tw); 
+end
+
+for j=1:length(t)
+    f.Ss(:,j) = p.sf(p.Sbottom, p.Stop, f.zs, f.zi(j));
+    f.Ts(:,j) = 0*f.zs+3; % 3 should be a user set parameter?
+end
+
+%% Iceberg forcing
+f.zi = (-p.H:10:0)'; % 10 should be a user set parameter?
+f.D = zeros(1,length(t));
+f.xi = (p.nu0/p.H)*exp(p.nu0*f.zi/p.H)/(1-exp(-p.nu0));
+
+%% Subglacial discharge forcing
+f.Qsg = zeros(1,length(t));
+f.Qsg(find(t>5)) = p.Qv0;
+
+end
