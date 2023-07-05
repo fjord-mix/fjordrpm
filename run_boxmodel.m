7d4aab69afcde121e3a4605dedb88022a086edd8
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-05 11:53:58 +0100

    updated driver file to run the box model

--- run_boxmodel.m
+++ run_boxmodel.m
@@ -1,36 +1,32 @@
-%% run_boxmodel.m
-% Description here
-
-%%
+% RUN_BOXMODEL Driver to run the box model simulation.
 clearvars
 close all
 
-%% Name the experiment and create an output directory
-
-%% Load the problem setup file (parameters, initial conditions, forcings)
-%% Examples
+%% Get the run parameters
+% Examples:
 addpath('examples/');
 % Example 1: intermediate_circulation
 % Example 2:
 % Example 3:
-p = intermediate_circulation;
+p = example_intermediate_circulation;
 
-%% time steps etc
+% Name the experiment and create a directory to store input parameters and
+% outputs
+name = 'EX_INT';
+mkdir(['./output_',name]);
+
+% Get the time variable, forcing structure and initial conditions structure
 t = 0:p.dt:p.t_end;
 f = get_forcing(p, t);
 a = get_initial_conditions(p, f);
 
-%% Run the model
-%     % run model
-% p.plot_runtime = 0;
-% s = boxmodel_v4(p,f,a,t);
-% 
-% 
-% % animate
-% 
-% %% and save the output
-% save INT.mat s p f
-% 
-% %% visualisations
-% 
-% animate_v4p1('INT.mat','INT_N6_withsill',50);
+% Save the input parameters
+save(['./output_',name,'/run_params.mat'], 'p', 'f', 't')
+
+%% Run the model (output is saved automatically)
+p.plot_runtime = 0;
+% loop through INDEX to allow multiple box model runs (i.e. to study parameter space)
+% can be a parfor loop
+for INDEX = 1:1 
+    boxmodel(p, f, a, t, name, INDEX);
+end
