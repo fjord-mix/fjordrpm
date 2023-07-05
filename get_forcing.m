998ebb2b1d309443403cacd0f64b4cd66d0e528d
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-05 09:29:18 +0100

    improved performance of get_forcing.m

--- get_forcing.m
+++ get_forcing.m
@@ -7,13 +7,13 @@ function f = get_forcing(p, t)
 %% Shelf forcing
 f.zs = -p.H:0; % will this break if p.H is not an integer?
 if p.zd == 0
-    f.zi = p.z0; % no oscillation
+    f.zi = p.z0*ones(size(t)); % no oscillation
 else
     f.zi = p.z0+(p.zd/2)*sin(2*pi*t/p.tw); % oscillation
 end
 [ZI, ZS] = meshgrid(f.zi, f.zs);
 f.Ss = p.sf(p.Sbottom, p.Stop, ZS, ZI);
-f.Ts1 = 0*ZS + 3; % 3 should be a user-set parameter? 
+f.Ts = 0*ZS + 3; % 3 should be a user-set parameter? 
 
 %% Iceberg forcing
 f.zi = (-p.H:10:0)'; % 10 should be a user set parameter?
@@ -22,6 +22,6 @@ f.xi = (p.nu0/p.H)*exp(p.nu0*f.zi/p.H)/(1-exp(-p.nu0));
 
 %% Subglacial discharge forcing
 f.Qsg = zeros(1,length(t));
-f.Qsg(find(t>5)) = p.Qv0; % 5 should be a user-set parameter? 
+f.Qsg(t>5) = p.Qv0; % 5 should be a user-set parameter? 
 
 end
