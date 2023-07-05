ea0d095af42e8fe289542abeb71316c0cafb196e
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-05 17:15:42 +0100

    updated int circ example with new structure

--- examples/example_intermediate_circulation.m
+++ examples/example_intermediate_circulation.m
@@ -1,4 +1,4 @@
-function p = example_intermediate_circulation
+function [p, a, f, t] = example_intermediate_circulation
 
 % EXAMPLE_INTERMEDIATE_CIRCULATION  Example to run the box model with intermediate circulation.
 %   P = EXAMPLE_INTERMEDIATE_CIRCULATION sets the user-defined box model parameters
@@ -24,23 +24,6 @@ p.N = 6; % number of above-sill model layers
 p.sill = 0; % flag for sill (0 = no sill, 1 = sill)
 p.silldepth = 500; % only used if p.sill=1
 
-%% Shelf parameters
-p.Stop = 30; % shelf salinity at the ocean surface
-p.Sbottom = 35; % shelf salinity at the ocean floor
-p.z0 = 50; % sets strength of shelf stratification
-p.zd = 30; % sets strength of shelf oscillation, zero if no oscillation in shelf
-p.tw = 10; % oscillation period of shelf forcing (days)
-p.sf = @(S1, S2, Z, Z0) S1 - (S1 - S2)*exp(Z./Z0); % functional form of the shelf stratification
-
-%% Glacier parameters
-p.Qv0 = 0; % volumetric flow rate of subglacial discharge
-p.P0 = 0; % plume entrainment width, 0 = no plume
-
-%% Iceberg parameters
-p.M0 = 0; % iceberg melt efficiency, 0 = no icebergs
-p.nu0 = 25; % iceberg volume profile coefficient
-p.E0 = 1e-7; % iceberg export efficiency
-
 %% Tuning/artificial parameters
 p.C0 = 1e4; % shelf exchange effiency
 p.K0 = 0.05; % vertical mixing effiency
@@ -52,5 +35,34 @@ p.Snudge = 0.*ones(1, p.N-1); % controls layer thickness for nudging, 0 if no nu
 %% Time step
 p.dt = 0.1; % time stepping (units are days)
 p.t_end = 10; % time to end the simulation
+t = 0:p.dt:p.t_end;
+
+%% Forcing parameters: idealised forcing
+p.z0 = 50; % sets strength of shelf stratification
+p.zd = 30; % sets strength of shelf oscillation, zero if no oscillation in shelf
+p.tw = 10; % oscillation period of shelf forcing (days)
+p.sf = @(S1, S2, Z, Z0) S1 - (S1 - S2)*exp(Z./Z0); % functional form of the shelf stratification
+p.Stop = 30; % shelf salinity at the ocean surface
+p.Sbottom = 35; % shelf salinity at the ocean floor
+p.Ttop = 3; % shelf temperature at the ocean surface
+p.Tbottom = 3; % shelf temperature at the ocean floor
+% Glacier forcing parameters
+p.Qv0 = 0; % volumetric flow rate of subglacial discharge
+p.P0 = 0; % plume entrainment width, 0 = no plume
+p.gf = @(T, Q) Q*ones(size(T)); % functional form of subglacial discharge rate depending on time
+% Iceberg forcing parameters
+p.M0 = 0; % iceberg melt efficiency, 0 = no icebergs
+p.nu0 = 25; % iceberg volume profile coefficient
+p.E0 = 1e-7; % iceberg export efficiency
+p.if = @(NU, H, Z) (NU/H)*exp(NU*Z/H)/(1-exp(-NU)); % functional form of iceberg depth profile    
+% forcing structure
+f = get_idealised_forcing(p, t);
+
+%% Forcing parameters: data-driven forcing
+% p.forcing_data = my_data;
+% f = get_data_forcing(p, t);
+
+%% Initial conditions
+a = get_initial_conditions(p, f);
 
 end
