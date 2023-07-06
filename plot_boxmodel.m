5e229def98f426a9f65b3c76a8363a6f6b04f074
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-06 13:35:16 +0100

    updated changes to plot box model

--- plot_boxmodel.m
+++ plot_boxmodel.m
@@ -1,16 +1,13 @@
 % PLOT_BOXMODEL Driver to plot the outputs of a particular box model simulation.
-clearvars
-close all
-
 %% Setup for the plots
-addpath('plots/');
+addpath('plotting_code/');
 % Select the box model simulation.
 name = 'EX_QSG';
 
 %% 1. Box model animation
-% Create a video of the box model simulation and save to the output
-% directory for the experiment
-nframeds = 50; % number of frames in the animation
-animatep1(name, nframes)
+% Create a video of the box model simulation showing temperature and salinity
+% and save to the output directory for the experiment.
+nframes = 50; % number of frames in the animation
+animate(name, nframes)
 
 
