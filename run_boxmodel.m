69d38eafeab55ef3520e03d6671e1998bf8cab15
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-07-06 16:53:41 +0100

    adding all changes

--- run_boxmodel.m
+++ run_boxmodel.m
@@ -5,19 +5,22 @@ addpath('simulation_code/')
 
 % Setup for the model run. Load the user-defined run parameters. 
 % Examples:
-addpath('example_runparameters/');
+addpath('example_run_parameters/');
 % 1. example_intermediate_circulation
 % 2. example_nudging
 % 3. example_subglacial_discharge 
-[p, a, f, t] = example_subglacial_discharge;
+%[p, a, f, t] = example_subglacial_discharge;
 % 4. example_data_driven
 % addpath('input_data_folder');
 % my_nice_data = process_data(input_data);
 % [p, a, ,f t] = example_data_driven(my_nice_data);
 
+% Tests:
+addpath('test_run_parameters/')
+[p, a, f, t] = test_changes;
 % Name the experiment and create a directory to store input parameters and
 % outputs.
-name = 'EX_SGD';
+name = 'TEST_CHANGES';
 mkdir(['./output_', name]);
 
 % Save the input parameters and initial conditions
