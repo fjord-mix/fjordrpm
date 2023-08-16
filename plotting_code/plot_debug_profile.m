cdbe597ef939ceb1144d7d389beae827ff84409d
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-08-16 15:19:33 +0100

    fixed time step mismatch between H and profiles

--- plotting_code/plot_debug_profile.m
+++ plotting_code/plot_debug_profile.m
@@ -2,7 +2,7 @@ function plot_debug_profile(i,t,f,p,H,S,s_bnds)
     % this is to monitor the how the fjord profile fares compared with the shelf
     % mainly for debugging purposes, which is why it's not a refined plot
     
-    ints = [0;-cumsum(H(:,i))]; z_levels=0.5*(ints(2:end)+ints(1:end-1));
+    ints = [0;-cumsum(H(:,i+1))]; z_levels=0.5*(ints(2:end)+ints(1:end-1));
     figure(99); 
     plot(f.Ss(:,i+1),f.zs); hold on; ylim([-p.H 0]); xlim(s_bnds);% to monitor interpolation results
     scatter(S(:,i+1),z_levels); yline(-cumsum(H(:,i+1)),':k','linewidth',0.5); % to monitor interpolation results  
