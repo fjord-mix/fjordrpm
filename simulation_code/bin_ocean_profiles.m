3a90a95cd7a3dbb6f676daf141b73d30d76a630e
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-09-26 14:34:02 +0100

    added surface (0m) to profile inter/extrapolation

--- simulation_code/bin_ocean_profiles.m
+++ simulation_code/bin_ocean_profiles.m
@@ -13,27 +13,33 @@ z  =  z(~nan_entries);
 Ss = flip(Sz);
 Ts = flip(Tz);
 zs = flip(z);
-zs0 = unique(sort([zs,-cumsum(H0)]));
+% zs0 = unique(sort([zs,-cumsum(H0)]));
+zs0 = unique(sort([0,zs,-cumsum(H0)]));
 
 Ss0 = interp1(zs,Ss,zs0,'pchip','extrap');
 Ts0 = interp1(zs,Ts,zs0,'pchip','extrap');
 
 [Te0,Se0] = deal(zeros(size(H0)));
 
+% for k=1:length(ints)-1
+%     inds = find(zs0<=ints(k) & zs0>=ints(k+1));
+%     if length(inds) == 1 % if there is only one data point, no need to average it
+%         Se0(k) = Ss0(inds);
+%         Te0(k) = Ts0(inds);
+%     elseif H0(k) < 1.1*p.Hmin
+%         % sometimes a very thin layer with sharp gradients will
+%         % not yield satisfactory results, so we use a simple average instead of numerical integration
+%         Se0(k) = mean(Ss0(inds));
+%         Te0(k) = mean(Ts0(inds));
+%     else
+%         Se0(k) = trapz(zs0(inds),Ss0(inds))/H0(k);
+%         Te0(k) = trapz(zs0(inds),Ts0(inds))/H0(k);
+%     end
+% end
 for k=1:length(ints)-1
     inds = find(zs0<=ints(k) & zs0>=ints(k+1));
-    if length(inds) == 1 % if there is only one data point, no need to average it
-        Se0(k) = Ss0(inds);
-        Te0(k) = Ts0(inds);
-    elseif H0(k) < 1.1*p.Hmin
-        % sometimes a very thin layer with sharp gradients will
-        % not yield satisfactory results, so we use a simple average instead of numerical integration
-        Se0(k) = mean(Ss0(inds));
-        Te0(k) = mean(Ts0(inds));
-    else
-        Se0(k) = trapz(zs0(inds),Ss0(inds))/H0(k);
-        Te0(k) = trapz(zs0(inds),Ts0(inds))/H0(k);
-    end
+    Se0(k) = trapz(zs0(inds),Ss0(inds))/H0(k);
+    Te0(k) = trapz(zs0(inds),Ts0(inds))/H0(k);
 end
 S0 = Se0';
 T0 = Te0';        
