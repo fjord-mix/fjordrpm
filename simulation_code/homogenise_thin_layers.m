2c9fecd95c6af1cd9e2be088a42e8ce3561e3102
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-10-04 17:27:30 +0100

    reversed order of OR statements

--- simulation_code/homogenise_thin_layers.m
+++ simulation_code/homogenise_thin_layers.m
@@ -8,11 +8,11 @@ H_tp1 = V_tp1/(W*L);
 
 for k=1:N
     % Only check up to the layer above the bottom.
-    if H_tp1(k) > 0 && k < N
+    if k < N && H_tp1(k) > 0 
         % If the layer will not collapse at the next timestep, timestep
         % forwards.
         continue
-    elseif H_tp1(k+1) > 0 && k < N 
+    elseif k< N && H_tp1(k+1) > 0 
         % Else, apply layer homogenisation for each layer k that is a
         % problem.
         homogenisation_flag = true;
@@ -20,11 +20,11 @@ for k=1:N
         Ht([k, k+1]) = (Ht(k) + Ht(k+1))/2;
         [Tt,St,Vt] = homogenise_layers(Vt, Tt, St,[k,k+1]);
         continue
-    elseif H_tp1(k+1) < 0 && k < N
+    elseif k < N && H_tp1(k+1) < 0 
         % If layer k+1 is also going to collapse, error because we have
         % 2 adjacent collapsing layers
         error("Error: two adjacent collapsing layers")
-    elseif H_tp1(k-1) > 0 && k == N
+    elseif k == N && H_tp1(k-1) > 0 
         % in the case that the collapsing layer is adjacent to the
         % bottom/sill, homogenise with the layer above.
         % (This layer is not collapsing or it would have errored already)
