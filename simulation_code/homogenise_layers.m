d357a6bad8694c588e5bfae613b0de4a178cfa66
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-10-06 15:59:47 +0100

    tidied up code

--- simulation_code/homogenise_layers.m
+++ simulation_code/homogenise_layers.m
@@ -5,8 +5,6 @@ Sh = S0;
 Vh = V0;
 VTh=VT0;
 VSh=VS0;
-% Th(i1) = (T0(i1).*V0(i1) + T0(i2).*V0(i2))./(V0(i1)+V0(i2));
-% Sh(i1) = (S0(i1).*V0(i1) + S0(i2).*V0(i2))./(V0(i1)+V0(i2));
 
 Th(inds) = sum(T0(inds).*V0(inds))./sum(V0(inds));
 Sh(inds) = sum(S0(inds).*V0(inds))./sum(V0(inds));
