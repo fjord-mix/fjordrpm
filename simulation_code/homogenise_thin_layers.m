421801cebb24fe8bb215d492578b9484cecaed79
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2023-10-06 08:59:45 +0100

    removed commented code

--- simulation_code/homogenise_thin_layers.m
+++ simulation_code/homogenise_thin_layers.m
@@ -8,26 +8,24 @@ H_tp1 = V_tp1/(W*L);
 
 for k=1:N
     % Only check up to the layer above the bottom.
-    if H_tp1(k) < 0 
-        if k< N && H_tp1(k) < 0 && H_tp1(k+1) > 0 
+    if H_tp1(k) <= 0 
+        if  k< N &&   H_tp1(k+1) > 0 
             % Else, apply layer homogenisation for each layer k that is a
             % problem.
             homogenisation_flag = true;
             % If layer k+1 is not going to collapse, homogenise k and k+1
-            %Ht([k, k+1]) = (Ht(k) + Ht(k+1))/2;
             [Tt,St,Vt,Ht] = homogenise_layers(Vt, Tt, St,[k,k+1],L,W);
             continue
-        elseif k < N && H_tp1(k) < 0&& H_tp1(k+1) < 0 
+        elseif  k < N &&  H_tp1(k+1) < 0 
             % If layer k+1 is also going to collapse, error because we have
             % 2 adjacent collapsing layers
             error("Error: two adjacent collapsing layers")
-        elseif k == N && H_tp1(k) < 0 && H_tp1(k-1) > 0 
+        elseif  k == N   && H_tp1(k-1) > 0 
             % in the case that the collapsing layer is adjacent to the
             % bottom/sill, homogenise with the layer above.
             % (This layer is not collapsing or it would have errored already)
             homogenisation_flag = true;
             % If layer k-1 is not going to collapse, homogenise k and k-1
-            %Ht([k-1, k]) = (Ht(k-1) + Ht(k))/2;
             [Tt,St,Vt,Ht] = homogenise_layers(Vt, Tt, St,[k-1,k],L, W);
             continue
         end
