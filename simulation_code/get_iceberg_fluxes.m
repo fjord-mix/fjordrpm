7eee164f5dc79469443a77f2868a8574df8d6109
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-02-20 06:54:59 +0000

    fixed error in calculating surface area

--- simulation_code/get_iceberg_fluxes.m
+++ simulation_code/get_iceberg_fluxes.m
@@ -15,13 +15,13 @@ if p.M0==0 % if no icebergs
     M0 = 0*zi;
 
 else
-    
+
     % melting in boxes
     ints = [0;-cumsum(H0)];
     zj = 0.5*(ints(1:end-1)+ints(2:end)); % mean depth of box
     Tf = p.l1*S0 + p.l2 + p.l3*zj; % local freezing point
 
-    % get vector of iceberg concentration that resolves box boundaries 
+    % get vector of iceberg concentration that resolves box boundaries
     zi0 = unique(sort([0,zi,-cumsum(H0)']));
     Ii0 = interp1(zi,I0,zi0,'pchip','extrap');
 
@@ -46,30 +46,30 @@ else
     % freshwater
     % Preallocate variables
     [QVmI, QTmI, QSmI, gmelt, Heff, SA_ice] =  deal(zeros(1, p.N+p.sill-1));
-    % loop over interfaces, allow plumes to rise from below sill layer 
+    % loop over interfaces, allow plumes to rise from below sill layer
     for k=1:p.N+p.sill-1
         % reduced gravity
         gmelt(k) = p.g*(p.betaS*S0(k)-p.betaT*T0(k)); % relative to the iceberg
         % effective height rise of plume in layer
         inds = find(zi0<=ints(k) & zi0>=ints(k+1));
         Heff(k) = trapz(zi0(inds),Ii0(inds));
-        % Surface area of icebergs to melt (assume tetrahedral shape)
-        SA_ice(k) = 6*sqrt(6)/(sqrt(4*Heff(k)^2/3)); 
+        % Surface area of icebergs to melt (assume characteristic length scale 400)
+        SA_ice(k) = p.W*p.L*6*sqrt(6)/400*Heff(k);
         % fluxes
-        if Heff(k) == 0 % no iceberg concentration
+        if Heff(k) == 0 % no iceberg concentration (e.g. on first timestep)
             QVmI(k) = 0;
-                  QTmI(k) = 0;
-        QSmI(k) = 0;
+            QTmI(k) = 0;
+            QSmI(k) = 0;
         elseif gmelt(k) < 0 % iceberg plume is denser than the box
             QVmI(k) = 0;
-               QTmI(k) = 0;
-        QSmI(k) = 0;
-        else 
+            QTmI(k) = 0;
+            QSmI(k) = 0;
+        else % iceberg plume which mixes into box above 
             QVmI(k) = p.gamma*QIi0(k+1)*p.alphaI^(2/3)*gmelt(k)^(1/3)*(SA_ice(k)/Heff(k))^(2/3)*Heff(k)/2;
-        QTmI(k) = QVmI(k)*T0(k+1)+p.gamma*QTi0(k+1);
-        QSmI(k) = QVmI(k)*S0(k+1)+p.gamma*QSi0(k+1);
+            QTmI(k) = QVmI(k)*T0(k+1)+p.gamma*QTi0(k+1);
+            QSmI(k) = QVmI(k)*S0(k+1)+p.gamma*QSi0(k+1);
         end
-        
+
     end
 
     % final fluxes
@@ -81,7 +81,7 @@ else
     %     QVmi0(p.N+p.sill) = 0;
     %     QTmi0(p.N+p.sill) = 0;
     %     QSmi0(p.N+p.sill) = 0;
-    % end        
+    % end
 
 
 end
