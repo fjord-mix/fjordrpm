50930d2cca90a02804553a1c4337a5696cd660b6
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-03 17:13:22 +0100

    new function to take stepping the zmodel forwards out of the main block of code

---
+++ simulation_code/step_zmodel_forwards.m
@@ -0,0 +1,14 @@
+function [VT, VS, T, S, I] = step_zmodel_forwards(p, f, V, VT, VS, I, M,...
+    QTg, QTs, QTk, QTi, QTmi, QTv, ...
+    QSg, QSs, QSk, QSi, QSmi, QSv)
+
+% Step the temperature, salt, heat content and salt content of the fjord forwards.
+    VT = VT +p.dt*p.sid*(QTg-QTs+QTk+QTi+QTmi+QTv);
+    VS = VS+p.dt*p.sid*(QSg-QSs+QSk+QSi+QSmi+QSv);
+    T = VT./V;
+    S = VS./V;
+
+    % Step icebergs forwards.
+    I = I+ (1-p.icestatic)*p.dt*p.sid*((f.D/(p.W*p.L))*f.xi-M.*I-p.uIce/p.L*I);
+
+end
