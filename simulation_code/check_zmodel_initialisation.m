fd537bf25dd481f94cc1c355eefb6b1967dff459
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-03 14:49:37 +0100

    added new function with error messages for checking the intialisatoion

---
+++ simulation_code/check_zmodel_initialisation.m
@@ -0,0 +1,24 @@
+function s = check_zmodel_initialisation(p, a)
+
+% Check shelf oscillation parameters have been set up correctly.
+if (isfield(p,'zd') && p.zd > 0) && (isfield(p,'tw') && p.tw  <= 0)
+    disp('Error: must have positive oscillation period if oscillation strength is set.')
+    s.status = 1; % Record an error and return error status.
+    return
+end
+
+% Check initialisation is consistent with specified number of layers.
+if any([length(a.H0) ~= p.N,length(a.T0) ~= p.N,length(a.S0) ~= p.N])
+    disp('Error: Initial conditions not consistent with number of layers');
+    s.status = 1; 
+    return
+end
+
+% Check sum of layer thicknesses is equal to fjord depth.
+if abs(sum(a.H0)-p.H) > 1e-10
+    disp('Error: box thicknesses must sum to fjord depth');
+    s.status = 1; 
+    return
+end
+
+end
