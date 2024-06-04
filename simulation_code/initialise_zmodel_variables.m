246c9688f9cf5cb9acdfe5335455618c09a1cdcf
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-04 09:12:03 +0100

    fixing errors in dimensions

--- simulation_code/initialise_zmodel_variables.m
+++ simulation_code/initialise_zmodel_variables.m
@@ -3,7 +3,7 @@ function [V, T, S, I, VT, VS,...
     QVk, QTk, QSk, QVmi, QTmi, QSmi,QIi,QTi,QSi,...
     QVv,QTv,QSv,Te,Se, phi, M] = initialise_zmodel_variables(p, f, a, t)
 
-[T, S, I] = deal(zeros(p.N, length(t)));
+[T, S] = deal(zeros(p.N, length(t)));
 
 [QVg,QTg,QSg,...
     QVs,QTs,QSs,...
@@ -13,11 +13,11 @@ function [V, T, S, I, VT, VS,...
     QVv,QTv,QSv,...
     Te,Se, phi] = deal(zeros(p.N,length(t)-1));
 
-M = zeros(length(f.zi),length(t)-1);
+[I, M] = deal(zeros(length(f.zi),length(t)-1));
 
 %% Initialise variables according with the boundary and initial conditions.
 
-V = a.H0*p.W*p.L; % volume of layers
+V = a.H0'*p.W*p.L; % volume of layers
 T(:,1) = a.T0; % temperature
 S(:,1) = a.S0; % salinity
 I(:,1) = a.I0; % iceberg concentration
