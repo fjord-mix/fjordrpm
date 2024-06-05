e2a4d3f274289825db38c70ccc73bb89bea1c4fc
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-05 10:25:37 +0100

    adding legend and axis labels

--- plotting_code/plot_runtime_profile.m
+++ plotting_code/plot_runtime_profile.m
@@ -1,5 +1,8 @@
-function plot_runtime_profile(i,t,f,p,H,S,T,t_bnds)
+function plot_runtime_profile(i,t,f,p,H,S,T,Se,QVs)
+
+
 s_bnds = [min(f.Ss(:)) max(f.Ss(:))+0.1];
+
    ints = -[0;cumsum(H(:,i))];
         z = f.zs;
         Sshelf = f.Ss(:,i);
@@ -14,9 +17,18 @@ s_bnds = [min(f.Ss(:)) max(f.Ss(:))+0.1];
         Uext(1) = QVs(end,i)/(p.W*H(end,i));
         % hf_track = monitor_boxmodel(hf_track,i,H,T,S,f);
         % hf_track = show_boxmodel([],i,t,H,T,S,QVs,QVg,QVk,QVb,f);
-        %         plot_debug_profile(i,t,f,p,H,S,[]);
+%         plot_debug_profile(i,t,f,p,H,S,[]);
         subplot(1,2,1);
-        plot(f.Ss(:,i),z,Sfjord,z,Sext,z); drawnow;
+        plot(f.Ss(:,i),z,Sfjord,z,Sext,z); 
+          set(gca,'box','on'); grid on;
+        xlabel('salinity');
+        ylabel('depth (m)');
+        legend('true shelf','shelf in box','fjord','location','southwest');
+        drawnow;
         subplot(1,2,2);
-        plot(Uext,z); drawnow;
+        plot(Uext,z); 
+        set(gca,'box','on'); grid on;
+        xlabel('exchange velocity (m/s)');
+        ylabel('depth (m)');
+        drawnow;
 end
