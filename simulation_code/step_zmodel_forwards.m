81fce4565a78e398e227ac1f1f28d42d37f4d60a
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-05 10:25:22 +0100

    including H and V

--- simulation_code/step_zmodel_forwards.m
+++ simulation_code/step_zmodel_forwards.m
@@ -1,12 +1,16 @@
-function [VT, VS, T, S, I] = step_zmodel_forwards(p, f, V, VT, VS, I, M, D,...
+function [V, VT, VS, H, T, S, I] = step_zmodel_forwards(p, f, V, VT, VS, I, M, D,...
+    QVg, QVs, QVk, QVmi, QVv, ...
     QTg, QTs, QTk, QTi, QTmi, QTv, ...
     QSg, QSs, QSk, QSi, QSmi, QSv)
 
 % Step the temperature, salt, heat content and salt content of the fjord forwards.
+    V  = V+p.dt*p.sid*(QVg-QVs+QVk+QVmi+QVv);
     VT = VT +p.dt*p.sid*(QTg-QTs+QTk+QTi+QTmi+QTv);
     VS = VS+p.dt*p.sid*(QSg-QSs+QSk+QSi+QSmi+QSv);
+    H = V/(p.W*p.L);
     T = VT./V;
     S = VS./V;
+
 
     % Step icebergs forwards.
     I = I+ (1-p.icestatic)*p.dt*p.sid*((D/(p.W*p.L))*f.xi-M.*I-p.uIce/p.L*I);
