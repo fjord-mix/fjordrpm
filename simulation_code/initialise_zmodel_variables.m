df9f39aeee2f6aa15f61712cdf3f5436807ed990
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-05 10:25:10 +0100

    including H and V

--- simulation_code/initialise_zmodel_variables.m
+++ simulation_code/initialise_zmodel_variables.m
@@ -1,9 +1,9 @@
-function [V, T, S, I, VT, VS,...
+function [H, V, T, S, I, VT, VS,...
     QVg, QTg, QSg, QVs, QTs, QSs,...
     QVk, QTk, QSk, QVmi, QTmi, QSmi,QIi,QTi,QSi,...
     QVv,QTv,QSv,Te,Se, phi, M] = initialise_zmodel_variables(p, f, a, t)
 
-[T, S] = deal(zeros(p.N, length(t)));
+[H, V, T, S] = deal(zeros(p.N, length(t)));
 
 [QVg,QTg,QSg,...
     QVs,QTs,QSs,...
@@ -16,12 +16,12 @@ function [V, T, S, I, VT, VS,...
 [I, M] = deal(zeros(length(f.zi),length(t)-1));
 
 %% Initialise variables according with the boundary and initial conditions.
-
-V = a.H0'*p.W*p.L; % volume of layers
+H(:,1) = a.H0;
+V(:,1) = a.H0'*p.W*p.L; % volume of layers
 T(:,1) = a.T0; % temperature
 S(:,1) = a.S0; % salinity
 I(:,1) = a.I0; % iceberg concentration
-VT(:,1) = V.*T(:,1); % heat content
-VS(:,1) = V.*S(:,1); % salt content
+VT(:,1) = V(:,1).*T(:,1); % heat content
+VS(:,1) = V(:,1).*S(:,1); % salt content
 
 end
