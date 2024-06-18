31c5932a05c79196f262ba4aa4955f9e8d8b5e62
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-18 11:23:29 +0100

    adding changes to tidying up code

--- simulation_code/get_zmodel_shelf_fluxes.m
+++ simulation_code/get_zmodel_shelf_fluxes.m
@@ -7,23 +7,19 @@ function [QVs0,QTs0,QSs0,Se0,Te0,phi0] = get_zmodel_shelf_fluxes(i, p, f, s)
 %   and solution S at timestep I.
 
 % Get tracer variables at timestep i.
-H0 = s.H(:,i);
-T0 = s.T(:,i);
-S0 = s.S(:,i);
+H0 = s.H(:,i); T0 = s.T(:,i); S0 = s.S(:,i);
 % Get boundary conditions at timestep i.
-Qsg0 = f.Qsg(i);
-zs = f.zs;
-Ts = f.Ts(:,i);
-Ss = f.Ss(:,i);
+Qsg0 = f.Qsg(i); zs = f.zs; Ts = f.Ts(:,i); Ss = f.Ss(:,i);
+% Initialise variables.
+[phi0, QVs0] = deal(zeros(p.N, 1));
 
 % Calculate the mean shelf T/S over box model layers above sill.
-[Te0, Se0] = bin_ocean_profiles(Ts,Ss,zs,H0',p);
+[Te0, Se0] = bin_zmodel_ocean_profiles(Ts, Ss, zs, H0);
 
 % Compute the fjord-to-shelf reduced gravity.
 gp = p.g*(p.betaS*(S0(1:s.ksill)-Se0(1:s.ksill))-p.betaT*(T0(1:s.ksill)-Te0(1:s.ksill)));
 
 % Calculate the potentials over above-sill layers.
-phi0 = zeros(size(s.phi(:,1)));
 phi0(1) = gp(1)*H0(1)/2;
 for k=2:s.ksill
     phi0(k) = phi0(k-1)+gp(k-1)*H0(k-1)/2+gp(k)*H0(k)/2;
@@ -39,10 +35,7 @@ Q = p.C0*p.W*H0(1:s.ksill).*phi0(1:s.ksill)/p.L;
 if p.P0==0
     Qsg0 = 0;
 end
-QVs0 = Q + H0(1:s.ksill)*(Qsg0-sum(Q))/sum(H0(1:s.ksill));
-
-% Fill any below-sill exchanges with zeros.
-QVs0(s.ksill+1:p.N) = 0;
+QVs0(1:s.ksill) = Q + H0(1:s.ksill)*(Qsg0-sum(Q))/sum(H0(1:s.ksill));
 
 % Compute the resulting heat/salt fluxes.
 QTs0 = (QVs0>=0).*QVs0.*T0 + (QVs0<0).*QVs0.*Te0;
