92af7634f6601107224fec2145367a52b49bdcca
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-18 16:01:16 +0100

    added help text and fixed indentation

--- simulation_code/check_zmodel.m
+++ simulation_code/check_zmodel.m
@@ -1,16 +1,22 @@
 function status = check_zmodel(p, H)
 
-    status = 0;
+% CHECK_ZMODEL Check for errors in the layer depths.
+%   STATUS = CHECK_ZMODEL(I, P, S) checks the zmodel layers H at timestep
+%   parameters P and returns an error status S with default value 0, or 1
+%   if there was an error.
 
-    % Check sum of layer thicknesses is equal to fjord depth.
-    if abs(sum(H)-p.H) > 1e-10
-        disp('Error: box thicknesses must sum to fjord depth');
-        status = 1; % status == 1 means there was an error
-    end
+status = 0;
 
-    if numel(uniquetol(H,1e-8)) > 1+p.sill
-        disp("Layer depths not all the same")
-        status = 1;
-    end
+% Check sum of layer thicknesses is equal to fjord depth.
+if abs(sum(H)-p.H) > 1e-10
+    disp('Error: box thicknesses must sum to fjord depth');
+    status = 1; %
+end
+
+% Check that the layer depths are not changing size.
+if numel(uniquetol(H,1e-8)) > 1+p.sill
+    disp("Layer depths not all the same")
+    status = 1;
+end
 
 end
