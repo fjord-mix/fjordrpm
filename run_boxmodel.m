6ae0cd84aba422495ff56afd4ccb0268844bbfd0
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-18 16:51:38 +0100

    updated and wrapped comments

--- run_boxmodel.m
+++ run_boxmodel.m
@@ -8,13 +8,11 @@ output_folder='./outputs'; % choose where to save your model outputs here
 
 
 %% Choosing the model example
-% Setup for the model run. Load the user-defined run parameters. 
-% Examples:
-% 1. Intermediate circulation
-% 2. Nudging based on target layers based on layer salinity
-% 3. Subglacial discharge
-% 4. Data-driven example: Kangerlussuaq during 2010-2018
-% 5. Data-driven example: Benchmark fjords during 2010-2018 using 3 layers
+% Setup for the model run. Load the user-defined run parameters. Examples:
+% 1. Intermediate circulation 2. Nudging based on target layers based on
+% layer salinity 3. Subglacial discharge 4. Data-driven example:
+% Kangerlussuaq during 2010-2018 5. Data-driven example: Benchmark fjords
+% during 2010-2018 using 3 layers
 %    above the sill (if exists).
 % 6. Data-driven example: Benchmark fjords during 2010-2018 using 2 layers
 %    above the sill (if exists).
@@ -22,10 +20,10 @@ output_folder='./outputs'; % choose where to save your model outputs here
 %    parametres (e.g., number of layers), which eventually causes the model
 %    to crash. Not everything is perfect!
 %
-%    The last three examples have 4 fjords ready to be run: 
-%    (1) Kangerlussuaq, (2) Sermilik, (3) Kangersuneq, (4) Ilulissat
-%    Be mindful that these are not 100% "plug and play". The user
-%    needs to choose which fjord from the fjord_model array to run
+%    The last three examples have 4 fjords ready to be run: (1)
+%    Kangerlussuaq, (2) Sermilik, (3) Kangersuneq, (4) Ilulissat Be mindful
+%    that these are not 100% "plug and play". The user needs to choose
+%    which fjord from the fjord_model array to run
 
 example_run = 8;
 which_fjord = 3; % used for example_run 5 to 7
@@ -56,29 +54,24 @@ switch example_run
     case 8
         load('boxmodel_example_bad_H_negT_lim.mat');
         fjord_run = cur_fjord;
-      %  fjord_run.a = [];
-
-
-%     fjord_run.p.dt    = 0; % time stepping (units are days)
-% fjord_run.p.t_end = 200; % time to end the simulation
-% fjord_run.t       = 0:fjord_run.p.dt:fjord_run.p.t_end;
         name = 'martim_crash';
     otherwise
         fjord_run = test_changes;
         name = 'test_fjord_model';
 end
 
-% use the plot runtime at your discretion. It substantially slows down the simulation, because it spends most of the time plotting!
+% Use the plot runtime at your discretion. It substantially slows down the
+% simulation, because it spends most of the time plotting!
 fjord_run.p.plot_runtime = 0; 
 fjord_run.m.name = name;
 
-% Set idealised boundary and initial conditions, if not given, based on input parameters.
-% Boundary conditions:
-if isempty(fjord_run.f), 
+% Set idealised boundary and initial conditions, if not given, based on
+% input parameters. Boundary conditions:
+if isempty(fjord_run.f)
     fjord_run.f = get_idealised_forcing(p, t); 
 end % we cannot use an OR statement here
 % Initial conditions:
-if isempty(fjord_run.a), 
+if isempty(fjord_run.a)
     fjord_run.a = get_initial_conditions(p, fjord_run.f); 
 end
 
@@ -87,9 +80,7 @@ if p.fixedthickness == 0
     [fjord_run.s,fjord_run.f] = boxmodel(fjord_run.p, fjord_run.t, fjord_run.f, fjord_run.a);
 elseif p.fixedthickness == 1
     % Run the code for fixed thickness layers.
-    tic
     [fjord_run.s,fjord_run.f] = zmodel(fjord_run.p, fjord_run.t, fjord_run.f, fjord_run.a);
-    toc
 end
 
 %% Saving results
@@ -100,11 +91,13 @@ if not(isfolder(output_folder))
     mkdir([output_folder,'/animations']);
 end
 
-% Save the fjord structure, including input parameters, initial conditions, and results
+% Save the fjord structure, including input parameters, initial conditions,
+% and results
 save([output_folder,'/model_results/', name, '.mat'], 'fjord_run')
 
 % create animation of the fjord run
-% animate([output_folder,'/model_results/', name, '.mat'],[output_folder,'/animations/'],name,50)
+% animate([output_folder,'/model_results/', name,
+% '.mat'],[output_folder,'/animations/'],name,50)
 
 % Example plot that can be generated from the model outputs
 % plot_outputs(fjord_run);
