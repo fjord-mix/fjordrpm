51221ff28a9a64f8af28029e0122864b0c85fa19
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-18 16:25:58 +0100

    added help text and wrapped comments

--- simulation_code/step_zmodel_forwards.m
+++ simulation_code/step_zmodel_forwards.m
@@ -1,14 +1,20 @@
 function Tr = step_zmodel_forwards(i, p, s, Q)
 
-    % Compute the zmodel tracer variables at timestep i+1.
-    Tr.V = s.V(:,i) +p.dt*p.sid*(Q.QVg-Q.QVs+Q.QVk+Q.QVmi+Q.QVv);
-    Tr.VT = s.VT(:,i)+p.dt*p.sid*(Q.QTg-Q.QTs+Q.QTk+Q.QTi+Q.QTmi+Q.QTv);
-    Tr.VS = s.VS(:,i)+p.dt*p.sid*(Q.QSg-Q.QSs+Q.QSk+Q.QSi+Q.QSmi+Q.QSv);
-    Tr.H = Tr.V/(p.W*p.L);
-    Tr.T = Tr.VT./Tr.V;
-    Tr.S = Tr.VS./Tr.V;
+% STEP_ZMODEL_FOWARDS Euler step for the zmodel simulation.
+%   TR = STEP_ZMODEL_FORWARDS(I, P, S, Q) performs an Euler step for the
+%   zmodel simulation for parameters structure P and given solution S and
+%   returns updated tracer variables stored in a structure TR.
 
-    % Step icebergs forwards.
-%     I = I+ (1-p.icestatic)*p.dt*p.sid*((D/(p.W*p.L))*f.xi-M.*I-p.uIce/p.L*I);
+% Compute the zmodel tracer variables at timestep i+1.
+Tr.V = s.V(:,i) +p.dt*p.sid*(Q.QVg-Q.QVs+Q.QVk+Q.QVmi+Q.QVv);
+Tr.VT = s.VT(:,i)+p.dt*p.sid*(Q.QTg-Q.QTs+Q.QTk+Q.QTi+Q.QTmi+Q.QTv);
+Tr.VS = s.VS(:,i)+p.dt*p.sid*(Q.QSg-Q.QSs+Q.QSk+Q.QSi+Q.QSmi+Q.QSv);
+Tr.H = Tr.V/(p.W*p.L);
+Tr.T = Tr.VT./Tr.V;
+Tr.S = Tr.VS./Tr.V;
+
+% Step icebergs forwards.
+%     I = I+
+%     (1-p.icestatic)*p.dt*p.sid*((D/(p.W*p.L))*f.xi-M.*I-p.uIce/p.L*I);
 
 end
