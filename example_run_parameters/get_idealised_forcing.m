35dcd715410588f85a862df6b8b4ab00e99c27b3
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-19 17:33:43 +0100

    tidied up help text and comments

--- example_run_parameters/get_idealised_forcing.m
+++ example_run_parameters/get_idealised_forcing.m
@@ -1,10 +1,12 @@
 function f = get_idealised_forcing(p, t)
 
-% GET_IDEALISED_FORCING  Obtain the forcing structure for an idealised input of functional form.
-%   F = GET_IDEALISED_FORCING(P, T) gets the forcing structure F for the shelf, icebergs and
-% subglacial discharge for the given box model parameters P and T.
+% GET_IDEALISED_FORCING  Obtain idealised boundary conditions in functional
+% form.
+%   F = GET_IDEALISED_FORCING(P, T) gets the forcing structure F for the
+%   shelf, icebergs and subglacial discharge for the given box model
+%   parameters P and T.
 
-%% Shelf forcing
+%% Get the functional form of the shelf forcing.
 f.zs = linspace(-p.H, 0, abs(p.H)+1); 
 if p.zd == 0
     f.zo = p.z0*ones(size(t)); % no oscillation
@@ -15,13 +17,13 @@ end
 f.Ss = p.sf(p.Sbottom, p.Stop, ZS, ZI);
 f.Ts = p.sf(p.Tbottom, p.Ttop, ZS, ZI); 
 
-%% Iceberg forcing
+%% Get the functional form of the iceberg forcing.
 f.zi = f.zs; 
 % f.D = p.df(t, p.D0);
 f.D = 0*t;
 f.xi = p.if(p.nu0, abs(p.zgl), f.zi)';
 
-%% Subglacial discharge forcing
+%% Get the functional form of the subglacial discharge forcing.
 f.Qsg = p.gf(t, p.Qv0);
 
 end
