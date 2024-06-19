51916b4a9aaa9f7fe76551f4a7d74bbe787cdbf7
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-06-19 12:29:57 +0100

    added time series for temperature

--- plotting_code/debug_series_plot.m
+++ plotting_code/debug_series_plot.m
@@ -1,5 +1,16 @@
-time1=1000;
-time2=2402;
+time1=1300;
+time2=i+1;
+
+figure('Position',[200 200 1000 500]);
+tiledlayout('flow')
+for k=1:10:60
+nexttile;
+plot(t(time1:time2),T(k:k+9,time1:time2))
+text(0.95,0.95,sprintf("layers %d to %d",k,k+9),'Units','normalized','HorizontalAlignment','right')
+xlabel('time (days)')
+ylabel('Temperature (^oC)')
+xlim([t(time1) t(time2)])
+end
 
 figure('Position',[200 200 1000 500]);
 tiledlayout('flow')
