23ac6bec6ba34c7e3655d898c0080e4403592fc7
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-19 17:21:59 +0100

    removed unused parameters, tidied up code

--- example_run_parameters/example_intermediate_circulation.m
+++ example_run_parameters/example_intermediate_circulation.m
@@ -1,27 +1,25 @@
 function run_output = example_intermediate_circulation
 
-% EXAMPLE_INTERMEDIATE_CIRCULATION  Example to run the box model with intermediate circulation.
-%   P = EXAMPLE_INTERMEDIATE_CIRCULATION sets the user-defined box model parameters
-%   P for the intermediate circulation example.
+% EXAMPLE_INTERMEDIATE_CIRCULATION  Example to run the z with intermediate circulation.
+%   RUN_OUTPUT = EXAMPLE_INTERMEDIATE_CIRCULATION returns a RUN_OUTPUT structure containing
+%   the user-defined zmodel parameters P and T for the icebergs example,
+%   along with the initial conditions A and boundary conditions F.
 
+%% Set the model default parameters.
 [p,~] = get_model_default_parameters();
 
-%% Fjord geometry parameters
-p.N = 6; % number of above-sill model layers
-p.sill = 0; % flag for sill (0 = no sill, 1 = sill)
+%% Set the specific parameter for this example.
+% Time values at which to compute the solution (in days).
+t  = 0:1:200;
 
-%% Tuning/artificial parameters
-p.Hmin = NaN; % minimum box thickness, NaN = no min thickness
-p.Snudge = 0.*ones(1, p.N-1); % controls layer thickness for nudging, 0 if no nudging
+% ZMODEL layer properties.
+p.N = 6; % number of above-sill model layers
 p.fixedthickness = 1; % fixed or variable layer thickness run
 
-%% Time step
-p.dt = 0.1; % time stepping (units are days)
-p.t_end = 1000; % time to end the simulation
-t = 0:p.dt:p.t_end;
+% Fjord geometry parameters
+p.sill = 0; % flag for sill (0 = no sill, 1 = sill)
 
-%% Forcing parameters: idealised forcing
-% Shelf forcing parameters
+% Shelf forcing parameters for idealised boundary conditions
 p.z0 = 50; % sets strength of shelf stratification
 p.zd = 30; % sets strength of shelf oscillation, zero if no oscillation in shelf
 p.tw = 10; % oscillation period of shelf forcing (days)
@@ -30,25 +28,26 @@ p.Stop = 30; % shelf salinity at the ocean surface
 p.Sbottom = 35; % shelf salinity at the ocean floor
 p.Ttop = 3; % shelf temperature at the ocean surface
 p.Tbottom = 3; % shelf temperature at the ocean floor
-% Glacier forcing parameters
+
+% Glacier forcing parameters for idealised boundary conditions
 p.Qv0 = 0; % volumetric flow rate of subglacial discharge
 p.P0 = 0; % plume entrainment width, 0 = no plume
 p.gf = @(T, Q) Q*ones(size(T)); % functional form of subglacial discharge rate depending on time
-% Iceberg forcing parameters
+
+% Iceberg forcing parameters for idealised boundary conditions
 p.M0 = 0; % iceberg melt efficiency, 0 = no icebergs
 p.nu0 = 25; % iceberg volume profile coefficient
 p.E0 = 1e-7; % iceberg export efficiency
 p.uIce = 0.005; % iceberg down-fjord velocity
 p.if = @(NU, H, Z) (NU/H)*exp(NU*Z/H)/(1-exp(-NU)); % functional form of iceberg depth profile    
 
-% forcing structure
-run_output.f = get_idealised_forcing(p, t);
-
-%% Initial conditions
-run_output.a = get_initial_conditions(p, run_output.f);
-
-%% adding the other structures to output structure
+%% Set the run output structure containing the inputted parameters.
 run_output.p = p;
 run_output.t = t;
+% Set the boundary conditions for the input parameters at each timestep.
+run_output.f = get_idealised_forcing(p, t);
+% Set the initial conditions for the input parameters with the initial
+% boundary conditions.
+run_output.a = get_initial_conditions(p, run_output.f);
 
 end
