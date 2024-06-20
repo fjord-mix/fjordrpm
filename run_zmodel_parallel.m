e14f13504e117da4a25df7d6e7062be8b16efff2
eleanorjohnstone <73657820+eleanorjohnstone@users.noreply.github.com>
2024-06-20 09:56:06 +0100

    fixed code so that it works with default parameters example; fixed/wrapped comments and fixed indentation

--- run_zmodel_parallel.m
+++ run_zmodel_parallel.m
@@ -1,42 +1,54 @@
-% RUN_ZMODEL Driver to run the zmodel simulation in parallel.
-% Examples: SGD-driven, constant shelf temp, stratified shelf salinity.
+% RUN_ZMODEL_PARALLEL Driver to run the zmodel simulation in parallel with
+% default parameters example varying the magnitude of the subglacial
+% discharge.
 
-num_workers=2; % how many processes in parallel will be run
-%% Set up parameter space
-experiment = 'sgd';
+%% Dealing with paths
+addpath(genpath('./'))
+clearvars
+close all
+% Choose where to save your model outputs.
+output_folder='./outputs';
+
+%% Set how many processes in parallel will be run.
+num_workers=2;
+
+%% Setup the model run.
+% Get the model parameters, initial conditions and boundary conditions for
+% the chosen example.
+[p0, t, f, a] = get_model_default_parameters;
+name = 'example_zmodel_parallel';
+% Choose the parameter to vary and the values it will take (code will run
+% in parallel for different values of this parameter).
 parameter_to_vary = 'Qv0';
 parameter_space = linspace(350, 1000, 50);
-t = linspace(1,100,1);
 
+% Create an empty output structure to store the results.
 if exist('fjord_par_outputs','Var'), clear fjord_par_outputs; end
 fjord_par_outputs(length(parameter_space)) = struct("p",[],"a",[],"f",[],"t",[],"m",[],"s",[]);
 
-p_def = get_model_default_parameters; % Get default parameters
-% setting up runs in serial significantly saves memory
+% Set up the runs in serial to save memory.
 for INDEX = 1:length(parameter_space)
     fjord_par_outputs(INDEX).m.name = sprintf('Iteration_%d\n', INDEX);
     fjord_par_outputs(INDEX).t = t;
-    fjord_par_outputs(INDEX).p = mod_run_param(parameter_to_vary, parameter_space(INDEX), p_def);
-    % Set idealised boundary and initial conditions, if not given, based on input parameters.
-    % Boundary conditions:
-    if isempty(fjord_par_outputs(INDEX).f)
-        fjord_par_outputs(INDEX).f = get_idealised_forcing(fjord_par_outputs(INDEX).p, t);
-    end % we cannot use an OR statement here
-    % Initial conditions
-    if isempty(fjord_par_outputs(INDEX).a)
-        fjord_par_outputs(INDEX).a = get_initial_conditions(fjord_par_outputs(INDEX).p, fjord_par_outputs(INDEX).f);
-    end
+    fjord_par_outputs(INDEX).p = mod_run_param(parameter_to_vary, parameter_space(INDEX), p0);
+    % Compute new boundary conditions and initial conditions based on the
+    % updated parameters p. Boundary conditions:
+    fjord_par_outputs(INDEX).f = get_idealised_forcing(fjord_par_outputs(INDEX).p, t);
+    % Initial conditions:
+    fjord_par_outputs(INDEX).a = get_initial_conditions(fjord_par_outputs(INDEX).p, fjord_par_outputs(INDEX).f);
 end
 
 %% Run the model (output is saved automatically).
-% starts parallel pool
-checkPool = gcp('nocreate'); % If no pool, do not create one
-if isempty(checkPool) % if there is no pool
+% If there is already a parallel pool running, do not create one.
+checkPool = gcp('nocreate');
+% If there is no pool, create one with the specified number of workers.
+if isempty(checkPool)
     parpool(num_workers);
 end
 
+% Run the zmodel in parallel.
 parfor INDEX = 1:length(parameter_space)
     [fjord_par_outputs(INDEX).s,fjord_par_outputs(INDEX).f] = ...
-        zmodel(fjord_par_outputs(INDEX).p, fjord_par_outputs(INDEX).t,[],[],[outs_path,'test_parallel/',fjord_par_outputs(INDEX).m.name,'.mat']);
+        zmodel(fjord_par_outputs(INDEX).p, fjord_par_outputs(INDEX).t,fjord_par_outputs(INDEX).f,fjord_par_outputs(INDEX).a,[output_folder,'/model_results/',fjord_par_outputs(INDEX).m.name,'.mat']);
     disp(fjord_par_outputs(INDEX).m.name)
 end
