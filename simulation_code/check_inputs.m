fd00c6c70e1910d981ff67461506834d21662b69
Donald Slater <donald.slater@ed.ac.uk>
2024-07-08 17:52:18 +0100

    Upgrading input checks from messages to actual errors

--- simulation_code/check_inputs.m
+++ simulation_code/check_inputs.m
@@ -7,23 +7,17 @@ status = 0;
 
 % Check provided depths are positive
 if any([p.Hsill<0, p.Hgl<0, p.H<0])
-    disp('Error: p.H, p.Hgl and p.Hsill must be positive');
-    status = 1;
-    return
+    error('p.H, p.Hgl and p.Hsill must be positive');
 end
 
 % Check dimensionality of initial conditions
-if ~isequal([size(a.H0)],[size(a.S0)],[size(a.T0)],[p.N,1])
-    disp('Error: initial conditions must have dimensions p.N x 1');
-    status = 1;
-    return
+if ~isequal([size(a.H0)],[size(a.S0)],[size(a.T0)],[size(a.I0)],[p.N,1])
+    error('Initial conditions (a.H0, a.S0, a.T0, a.I0) must have dimensions p.N x 1');
 end
 
 % Check sum of layer thicknesses is equal to fjord depth
 if abs(sum(a.H0)-p.H) > 1e-10
-    disp('Error: layer thicknesses (a.H0) must sum to fjord depth (p.H)');
-    status = 1; 
-    return
+    error('Layer thicknesses (a.H0) must sum to fjord depth (p.H)');
 end
 
 % Check dimensionality of forcings
@@ -31,35 +25,29 @@ nz = length(f.zs);
 nt = length(t);
 % f.zs
 if ~isequal(size(f.zs),[nz,1])
-    disp('Error: f.zs must have dimensions nz x 1');
-    status = 1;
-    return
+    error('f.zs must have dimensions nz x 1');
 end
 % f.Ss and f.Ts
 if ~isequal(size(f.Ss),size(f.Ts),[nz,nt])
-    disp('Error: f.Ss and f.Ts must have dimensions nz x nt');
-    status = 1;
-    return
+    error('f.Ss and f.Ts must have dimensions nz x nt');
 end
 % f.Qsg
 if ~isequal(size(f.Qsg),[1,nt])
-    disp('Error: f.Qsg must have dimensions 1 x nt');
-    status = 1;
-    return
+    error('f.Qsg must have dimensions 1 x nt');
 end
 
-% If t_save is not given, it will take t instead
-if ~isfield(p,'t_save')
-    p.t_save=t;
-end
-
-% Check that the time values where the solution will be saved are a subset
-% of the time values where the solution will be computed
-if ~all(ismember(p.t_save, t)) 
-    disp("Error: the values where the solution is saved, p.t_save, must be a subset of the values where the solution is computed, t.")
-    status = 1;
-    return
-end
+% % If t_save is not given, it will take t instead
+% if ~isfield(p,'t_save')
+%     p.t_save=t;
+% end
+% 
+% % Check that the time values where the solution will be saved are a subset
+% % of the time values where the solution will be computed
+% if ~all(ismember(p.t_save, t)) 
+%     disp("Error: the values where the solution is saved, p.t_save, must be a subset of the values where the solution is computed, t.")
+%     status = 1;
+%     return
+% end
 
 end
 
