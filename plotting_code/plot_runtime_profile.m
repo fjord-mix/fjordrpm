bdb27f0523b9454335d99a6616f0f3f06dbf89c1
Donald Slater <donald.slater@ed.ac.uk>
2024-07-09 10:22:10 +0100

    Updating plot_runtime_profile

--- plotting_code/plot_runtime_profile.m
+++ plotting_code/plot_runtime_profile.m
@@ -1,40 +1,32 @@
 function plot_runtime_profile(i, p, t, f, s)
 
-% Preallocate variables
-H = s.H; S = s.S; Se = s.Se; QVs = s.QVs; z = f.zs;
-Sfjord = zeros(size(H,1));
-Sext = zeros(size(H,1));
-Uext = zeros(size(H,1));
+% Plot temperatures
+ints = -[0;cumsum(s.H)];
+subplot(1,3,1); cla; hold on;
+plot(f.Ts(:,i),f.zs,'linewidth',2);
+stairs([s.T(1,i);s.T(:,i)],ints,'linewidth',2);
+set(gca,'box','on'); grid on;
+xlabel('temperature (C)');
+ylabel('depth (m)');
+legend('shelf','fjord','location','southwest');
 
-% Find the box and shelf salinity and fjord-shelf velocity in each layer.
-ints = -[0;cumsum(H(:,i))];
-for k=1:size(H,1)
-    inds=find(z<=ints(k)&z>=ints(k+1));
-    Sfjord(inds)=S(k,i);
-    Sext(inds)=Se(k,i);
-    Uext(inds)=QVs(k,i)/(p.W*H(k,i));
-end
-
-Sfjord(1) = S(end,i);
-Sext(1) = Se(end,i);
-Uext(1) = QVs(end,i)/(p.W*H(end,i));
-
-%  Plot the shelf salinity and the shelf/box salinity in each layer.
-subplot(1,2,1);
-plot(f.Ss(:,i),z,Sext,z,Sfjord,z);
+% Plot salinities
+ints = -[0;cumsum(s.H)];
+subplot(1,3,2); cla; hold on;
+plot(f.Ss(:,i),f.zs,'linewidth',2);
+stairs([s.S(1,i);s.S(:,i)],ints,'linewidth',2);
 set(gca,'box','on'); grid on;
 xlabel('salinity');
 ylabel('depth (m)');
-legend('true shelf','shelf in box','fjord','location','southwest');
-drawnow;
+title(['t = ',num2str(0.01*round(100*t(i))),' days']);
 
-% Plot the fjord-shelf velocity in each layer.
-subplot(1,2,2);
-plot(Uext,z);
+% Plot fjord-shelf exchange velocity
+subplot(1,3,3);
+stairs(-[s.QVs(1,i)/(p.W*s.H(1));s.QVs(:,i)./(p.W*s.H)],ints,'k','linewidth',2);
 set(gca,'box','on'); grid on;
-xlabel('exchange velocity (m/s)');
+xlabel('fjord-shelf exchange velocity (m/s)');
 ylabel('depth (m)');
-text(gca,0.05,0.05,['t = ',num2str(0.01*round(100*t(i+1))),' days'],'fontsize',12,'VerticalAlignment','bottom','units','normalized');
+title('positive values directed out of fjord');
 drawnow;
 
 end
